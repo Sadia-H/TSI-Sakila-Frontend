59e1fa5f7aa6025c92daf7b291e3e68d6d0f4c9f
Sadia-H <sadiahussain118@gmail.com>
2024-07-25 09:54:47 +0100

    Added React-Router-dom in App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import './App.css'
-import AllActors from './Components/AllActors'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import AllFilms from './Components/AllFilms'
 import FilmById from './Components/FilmByID'
-import Navbar from './Components/Navbar'
 
 function App() {
-  // const filmId = 1; 
   return (
-    <>
-      <h1>hello</h1>
-      {/* <Navbar/> */}
-      {/* <AllActors/> */}
-      {/* <AllFilms/> */}
-      {/* <FilmById filmId={filmId} /> */}
-      <FilmById />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/film-by-id' element={<FilmById/>} />
+        <Route path='/all-films' element = {<AllFilms/>} />
+      </Routes>
+
+    </BrowserRouter>
         
-    </>
+  
   )
 }
 
